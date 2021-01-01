15473e116fc55727bdc3cbce2545375d0e66728b
Beniamino Green <ben@greendalba.com>
2021-01-01 13:34:33 -0800

    re-did cragg tests so they no longer rely on external data

--- tests/testthat/test-cragg_donald.R
+++ tests/testthat/test-cragg_donald.R
@@ -1,12 +1,26 @@
-#Todo: Add more tests cases for which the Cragg-Donald stats aren't
-#	equivalent to the Wald-test statistic
 test_that("cragg_donald results are accurate", {
-	results <- read.csv("./test-data/stata_testing_data.csv", header=F)[,2]
-	for (i in seq(10)) {
-		data <- read.csv(paste0("./test-data/data_", i ,".csv"))
-		expect_equal(round(cragg_donald(~X1 + X2 + X3,~D,~Z1 + Z2, data)$cd_stat,3), results[i])
+	fake_data <- function (seed,n) {
+		set.seed(seed)
+		X1 = runif(n)
+		XA = X1*runif(n)
+		X2 = runif(n)
+		XB = X2*runif(n)
+		Z1 = runif(n)
+		ZA = Z1*runif(n)
+		Z2 = runif(n)
+		ZB = Z2*runif(n)
+		Z3 = runif(n)
+		ZC = Z3*runif(n)
+		D1 = XA + 5*ZA +ZB
+		D2 = XB + 2*ZB + ZC
+		Y = XB + D1*2*runif(n) + D2*runif(n)
+		data.frame(X1,X2,Z1,Z2,Z3,Y,D1,D2)
 	}
-	data <- read.csv("./test-data/data_1.csv")
-	expect_equal(round(cragg_donald(~1, ~D, ~Z1 + Z2, data = data)$cd_stat,3),204.916)
-	expect_equal(round(cragg_donald(~X1, ~ZA+X2, ~ epsilon + X3 , data = data )$cd_stat,3), .042)
+
+	answers <- c(41.614, 52.252, 33.711, 44.734,39.979,30.105,44.939,27.960,30.532,31.191)
+	for (i in seq(20,29)) {
+		ans<- answers[i-19]
+		cd_stat <- cragg_donald(~X1, ~D1 + D2 ,~ Z1 + Z2 +Z3 , data = fake_data(i,250))$cd_stat
+		expect_equal(round(cd_stat,3), ans)
+	}
 })
